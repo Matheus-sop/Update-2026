5556b2c05e4288be663a9b7cd9b3e204661f990c
Matheus Silva <matheus.sop@gmail.com>
2026-04-24 10:27:48 -0300

    Simple Create Form

--- WebApplication2/Controllers/SellersController.cs
+++ WebApplication2/Controllers/SellersController.cs
@@ -1,19 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication2.Models;
 using WebApplication2.Services;
 
 namespace WebApplication2.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly WebApplication2Context _context;
+        
+        
         private readonly SellerService _sellerService;
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, WebApplication2Context context)
         {
             _sellerService = sellerService;
+            _context = context;
         }
         public IActionResult Index()
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            var departments = _context.Department.Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Name
+                      
+            }).ToList();
+
+
+            //var departmentsList = new List<SelectListItem>();
+            //foreach (var department in _context.Department)
+            //{
+            //    var departmentItem = new SelectListItem(department.Name, department.Id.ToString());
+            //    departmentsList.Add(departmentItem);
+            //}
+
+            ViewBag.Departments = new SelectList(_context.Department.ToList(), "Id", "Name");   
+            //List<Department> departments = new List<Department>();
+
+            //departments = _context.Department.ToList();
+
+            //ViewBag.Departments = new SelectList(departments, "Id", "Name");
+            return View();
+        }
+    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            var department = _context.Department.Where(d => d.Id == seller.Department.Id).FirstOrDefault();
+            seller.Department = department;
+
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- WebApplication2/Services/SellerService.cs
+++ WebApplication2/Services/SellerService.cs
@@ -13,5 +13,10 @@ namespace WebApplication2.Services
         {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
